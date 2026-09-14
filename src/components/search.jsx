818a5115a52aea51ea975c38cbb9910e950bbea5
melili-bloxter <melvinagadaxp2@gmail.com>
2026-09-14 11:13:10 +0100

    first commit

---
+++ src/components/search.jsx
@@ -0,0 +1,19 @@
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+    <div>
+      <img src="search.svg" alt="search" />
+      <input
+        type="text"
+        placeholder="Search for a movie"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+    </div>
+    </div>
+  )
+}
+
+
+
+export default Search
